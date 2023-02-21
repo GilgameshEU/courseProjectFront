b9076e53255e3b64ab0f284c434c440a4708c1a4
GilgameshEU <d1vergence@yandex.ru>
2023-02-21 23:05:37 +0400

    collections

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useStyles } from "../styles";
 import { API_URL } from "./Login";
 import axios from "axios";
-import { Typography } from "@mui/material";
+import { Typography, IconButton } from "@mui/material";
+import { Favorite, Share } from "@mui/icons-material";
 
 const Home = () => {
   const classes = useStyles();
@@ -25,12 +26,25 @@ const Home = () => {
     <div className={classes.root}>
       <div style={{ display: "flex", flexDirection: "column" }}>
         {collections.map((collection) => (
-          <div key={collection.id} style={{ marginBottom: "16px" }}>
-            <Typography variant="h6">{collection.name}</Typography>
-            <Typography variant="body1" dangerouslySetInnerHTML={{ __html: collection.descriptionHtml }} />
-            <Typography variant="body2">Theme: {collection.theme}</Typography>
-            <Typography variant="body2">Created At: {new Date(collection.createdAt).toLocaleString()}</Typography>
-            <Typography variant="body2">User: {collection.user.name}</Typography>
+          <div key={collection.id} style={{ marginBottom: "16px", display: "flex", alignItems: "center" }}>
+            <img src={collection.image} alt="Collection" style={{ marginRight: "16px", width: "80px", height: "80px", objectFit: "cover" }} />
+            <div style={{ flex: 1 }}>
+              <Typography variant="h6">{collection.name}</Typography>
+              <Typography variant="body2">Theme: {collection.theme}</Typography>
+              <Typography variant="body2">Created At: {new Date(collection.createdAt).toLocaleString()}</Typography>
+              <Typography variant="body2">User: {collection.user.name}</Typography>
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <IconButton>
+                  <Favorite />
+                </IconButton>
+                <IconButton>
+                  <Share />
+                </IconButton>
+                <a href={`/collections/${collection.id}`} style={{ marginLeft: "auto" }}>
+                  Подробнее
+                </a>
+              </div>
+            </div>
           </div>
         ))}
       </div>
